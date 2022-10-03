60be4ecd1d27ea117a0d2cd0dc045d4174eadb75
JpScoot <jamespjones0714@gmail.com>
2022-10-03 14:49:54 -0700

    Upgraded Grid class to ArrayList.

--- src/main/java/com/tlglearning/tictactoe/Grid.java
+++ src/main/java/com/tlglearning/tictactoe/Grid.java
@@ -1,21 +1,19 @@
 package com.tlglearning.tictactoe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *  This class prints out a 3 x 3 console representation of the Tic-Tac-Toe grid.
  */
 public class Grid {
-
-  String square1 = new String(" ");
-  String square2 = new String(" ");
-  String square3 = new String(" ");
-  String square4 = new String(" ");
-  String square5 = new String(" ");
-  String square6 = new String(" ");
-  String square7 = new String(" ");
-  String square8 = new String(" ");
-  String square9 = new String(" ");
+  private List<String> squares = new ArrayList<>();
 
   public Grid() {
+    for(int i =0; i < 9; i++){
+      squares.add(" ");
+
+    }
 
   }
 
@@ -23,11 +21,12 @@ public class Grid {
    * Prints out a representation of the grid in a specific state.
    */
   public void gridLayout() {
-    System.out.printf(" %s | %s | %s%n", square7, square8, square9);
+    System.out.printf(" %s | %s | %s%n", squares.get(6), squares.get(7), squares.get(8));
     System.out.println("-----------");
-    System.out.printf(" %s | %s | %s%n", square4, square5, square6);
+    System.out.printf(" %s | %s | %s%n", squares.get(3), squares.get(4), squares.get(5));
     System.out.println("-----------");
-    System.out.printf(" %s | %s | %s%n", square1, square2, square3);
+    System.out.printf(" %s | %s | %s%n", squares.get(0), squares.get(1),squares.get(2));
+
 
   }
 
