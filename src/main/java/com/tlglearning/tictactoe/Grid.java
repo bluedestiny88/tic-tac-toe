1c853a28f8f21b48d6f490208aeb573c1a3a6387
bluedestiny88 <bluedestiny88@gmail.com>
2022-10-03 12:13:15 -0700

    Fixed code

--- src/main/java/com/tlglearning/tictactoe/Grid.java
+++ src/main/java/com/tlglearning/tictactoe/Grid.java
@@ -5,10 +5,4 @@ package com.tlglearning.tictactoe;
  */
 public class Grid {
 
-  public static void main(String[] args) {
-    // TODO: 10/3/2022 Print out a 3x3 representation of the Tic-Tac-Toe grid in
-    //  console. Leave room in the squares to accept a String representation of
-    //  X and O.
-  }
-
 }
