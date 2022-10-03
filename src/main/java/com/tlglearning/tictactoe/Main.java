d08482e0e55580cf239dcbd62d25812b3a66179f
bluedestiny88 <bluedestiny88@gmail.com>
2022-10-03 14:06:12 -0700

    Added the enterKey method and walked through a demo with the help of Nick.

--- src/main/java/com/tlglearning/tictactoe/Main.java
+++ src/main/java/com/tlglearning/tictactoe/Main.java
@@ -1,13 +1,51 @@
 package com.tlglearning.tictactoe;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
- *  The focal point of where the game will run from and check to see if the game
- *  is finished using various logics.
+ * The focal point of where the game will run from and check to see if the game is finished using
+ * various logics.
  */
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Grid grid1 = new Grid();
     grid1.gridLayout();
+    //  Lines 22 - 26 are for demonstration purposes only, will be replaced in further
+    //  implementations
+    Set<Integer> fakeSet = IntStream
+        .rangeClosed(1, 9)
+        .boxed()
+        .collect(Collectors.toCollection(TreeSet::new));
+    enterKey(fakeSet);
+
+  }
+
+  public static int enterKey(Set<Integer> validSpaces) throws IOException {
+    int userNumber = 0;
+    Reader input = new InputStreamReader(System.in);
+    BufferedReader buffer = new BufferedReader(input);
+    do {
+      System.out.printf("Please enter a space number %s: ",
+          validSpaces); //  consider passing in a treeSet
+      try {
+        userNumber = Integer.parseInt(buffer.readLine().trim());
+        if (!validSpaces.contains(userNumber)) {
+          throw new IllegalArgumentException();
+        }
+        break;
+      } catch (IllegalArgumentException e) {
+        System.out.println("Sorry, that is not an available space!");
+
+      }
+    } while (true); //  doing 0 ends the
+    return userNumber;
   }
 }
