97a042ae8ef211c3ab8b5aa59120be8e0a46a8a7
bluedestiny88 <bluedestiny88@gmail.com>
2022-10-05 12:04:28 -0700

    Added in logic to get the game interactable, but ran into a compile time error after player 1 runs.

--- src/main/java/com/tlglearning/tictactoe/Grid.java
+++ src/main/java/com/tlglearning/tictactoe/Grid.java
@@ -1,27 +1,55 @@
 package com.tlglearning.tictactoe;
 
+import com.tlglearning.tictactoe.users.OPlayer;
+import com.tlglearning.tictactoe.users.XPlayer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  *  This class prints out a 3 x 3 console representation of the Tic-Tac-Toe grid.
  */
 public class Grid {
-  private List<Integer> squares = new ArrayList<>();
+  private List<String> squares = new ArrayList<>();
+  private Set<Integer> availableSpaces = IntStream
+      .rangeClosed(1, GRID_SIZE)
+      .boxed()
+      .collect(Collectors.toCollection(TreeSet::new));
+  private Player player1 = new XPlayer();
+  private Player player2 = new OPlayer();
   private static final int GRID_SIZE = 9;
 
   /**
    *
    */
   public Grid() {
-    for(int i =0; i < GRID_SIZE; i++){
-      squares.add(i+1);
-
+    for(int i = 0; i < GRID_SIZE; i++){
+      Integer stringNum = i + 1;
+      squares.add(stringNum.toString());
     }
   }
 
-  public List<Integer> getGrid() {
+  public List<String> getGrid() {
     return squares;
+  }
+
+  public Set<Integer> getAvailableSpaces()  {
+    return availableSpaces;
+  }
+
+  public Player getXPlayer()  {
+    return player1;
+  }
+
+  public Player getOPlayer()  {
+    return player2;
+  }
+
+  public void setAvailableSpaces(int spaceToRemove)  {
+    availableSpaces.remove(spaceToRemove);
   }
 
   /**
--- src/main/java/com/tlglearning/tictactoe/Main.java
+++ src/main/java/com/tlglearning/tictactoe/Main.java
@@ -4,13 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import org.w3c.dom.ls.LSOutput;
 
 /**
  * The focal point of where the game will run from and check to see if the game is finished using
@@ -23,15 +21,29 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     Grid grid1 = new Grid();
-    grid1.gridLayout();
-
+    boolean gameIsRunning = true;
+    boolean isPlayerXsTurn = true;
     //  Lines 22 - 26 are for demonstration purposes only, will be replaced in further
     //  implementations
-    Set<Integer> fakeSet = IntStream
-        .rangeClosed(1, GRID_SIZE)
-        .boxed()
-        .collect(Collectors.toCollection(TreeSet::new));
-    enterKey(fakeSet);
+    System.out.println("Welcome to Tic-Tac-Toe.");
+    do {
+      grid1.gridLayout();
+      if (isPlayerXsTurn) {
+        System.out.println(grid1.getXPlayer().getPlayerStart());  //  Print out "Player X, it is your turn: "
+        int chosenNum = enterKey(grid1.getAvailableSpaces()) - 1; //  Player to choose a square
+        grid1.getGrid().set(chosenNum, grid1.getXPlayer().getPlayer());
+        grid1.setAvailableSpaces(chosenNum);
+      } else  {
+        System.out.println(grid1.getOPlayer().getPlayerStart());  //  Print out "Player X, it is your turn: "
+        int chosenNum = enterKey(grid1.getAvailableSpaces()) - 1; //  Player to choose a square
+        grid1.getGrid().add(chosenNum, grid1.getOPlayer().getPlayer());
+        grid1.setAvailableSpaces(chosenNum);
+      }
+      gameIsRunning = checkWinner(grid1);
+      gameIsRunning = checkDraw(grid1);
+      isPlayerXsTurn = !isPlayerXsTurn;
+    } while(gameIsRunning);
+    // enterKey(grid1.getAvailableSpaces());
 
   }
 
@@ -40,7 +52,6 @@ public class Main {
     int userNumber = 0;
     Reader input = new InputStreamReader(System.in);
     BufferedReader buffer = new BufferedReader(input);
-
     do {
       System.out.printf("Please enter a space number %s: ",
           validSpaces); //  consider passing in a treeSet
@@ -61,7 +72,8 @@ public class Main {
    * This method checks to see if tic-tac-toe has a winner.
    * @param table - a representation of the Grid layout.
    */
-  public void checkWinner(Grid table) {
+  public static boolean checkWinner(Grid table) {
+    boolean thereIsaWinner = false;
     for (int i = 0; i < LINE_TOTAL_WINS; i++) {
       String line = null;
       switch (i + 1) {
@@ -100,18 +112,20 @@ public class Main {
       }
       if(line.equals("XXX")) {
         System.out.println("X, You are the winner!");
+        thereIsaWinner = true;
         break;
       }
        if(line.equals("OOO")) {
         System.out.println("O, You are the winner!");
+        thereIsaWinner = true;
         break;
       }
     }
+    return thereIsaWinner;
   }
 
-
-  public boolean checkDraw(Grid table){
-    List<Integer> grid = table.getGrid();
+  public static boolean checkDraw(Grid table){
+    List<String> grid = table.getGrid();
     boolean stillPlaying = false;
     for(int i = 0; i < GRID_SIZE; i++){
       if( !grid.get(i+1).equals("X")  && !grid.get(i+1).equals("O")){
